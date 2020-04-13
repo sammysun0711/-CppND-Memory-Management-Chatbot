b772865ca83d12993eb8e53e36d87c4ea88d5ff9
Xiake <xiakesun0711@gmail.com>
2020-04-13 22:37:21 +0200

    Bugfix: fix copy constructor and copy assginment

--- src/chatbot.cpp
+++ src/chatbot.cpp
@@ -48,32 +48,31 @@ ChatBot::~ChatBot()
 // Copy Constructor
 ChatBot::ChatBot(const ChatBot &source)
 {
-    //std::cout << "ChatBot Copy Constructor" << std::endl;
     std::cout << "COPYING content of instance " << &source << " to instance " << this << std::endl;
-    *_currentNode = *source._currentNode;
-    *_rootNode = *source._rootNode;
-    *_chatLogic = *source._chatLogic;
+    _currentNode = source._currentNode;
+    _rootNode = source._rootNode;
+    _chatLogic = source._chatLogic;
     _image = new wxBitmap(*source._image);
 }
 
-ChatBot& ChatBot::operator=(const ChatBot &source){
+// Copy Assiginment
+ChatBot& ChatBot::operator=(const ChatBot &source)
+{
     std::cout << "ASSIGNING content of instance " << &source << " to instance " << this << std::endl;
-    //std::cout << "ChatBot Copy Assignment" << std::endl;
     if (this == &source)
         return *this;
     delete _image;
     _image = new wxBitmap(*source._image);
-    *_currentNode = *source._currentNode;
-    *_rootNode = *source._rootNode;
-    *_chatLogic = *source._chatLogic;
+    _currentNode = source._currentNode;
+    _rootNode = source._rootNode;
+    _chatLogic = source._chatLogic;
     return *this;
 }
 
+// Move Constructor
 ChatBot::ChatBot(ChatBot &&source)
 {
-    //std::cout << "ChatBot Move Constructor" << std::endl;
     std::cout << "MOVING (constructor) instance " << &source << " to instance " << this << std::endl;
-    //_image = std::move(source._image);
     _image = source._image;
     _currentNode = source._currentNode;
     _rootNode = source._rootNode;
@@ -84,15 +83,14 @@ ChatBot::ChatBot(ChatBot &&source)
     source._chatLogic = nullptr;
 }
 
+// Move Assiginment
 ChatBot& ChatBot::operator=(ChatBot &&source)
 {
-    //std::cout << "ChatBot Move Assginment" << std::endl;
     std::cout << "MOVING (assign) instance " << &source << " to instance " << this << std::endl;
     if (this == &source)
         return *this;
     delete _image;
     _image = source._image;
-    //_image = std::move(source._image);
     _currentNode = source._currentNode;
     _rootNode = source._rootNode;
     _chatLogic = source._chatLogic;
